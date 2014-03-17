b78c483007c0765e2636bf05196b14b18927b159
MortenHofft <mortenhoefft@gmail.com>
2014-03-17 23:24:12 +0100

    angular post generation introduced

---
+++ content.js
@@ -0,0 +1,44 @@
+var posts = [
+    {type:"Projects",
+     date:"March 17, 2014",
+     machinedate: "20140317",
+     title:"Quickselect",
+     description:"The median is often a useful number. In many cases it can describe data better than the average, as it is resistant to outliers and contaminated data in general. But it is expensive to sort the entire array. Instead we can use an algorithm dedicated to finding the n-th largest number - namely Hoare's selection algorithm Quickselect.",
+     link:"quickselect"
+    },
+    {type:"Projects",
+     date:"March 2, 2014",
+     machinedate: "20140302",
+     title:"Object tracking",
+     description:"An algorithm for tracking objects as they move around in a video sequence. It is a JavaScript implementation and works real time, so you can try it using your webcam. Or just read about the workings and play with the interactive example.",
+     link:"medianflow"
+    },
+    {type:"Miscellaneous",
+     date:"March 1, 2014",
+     machinedate: "20140301",
+     title:"Cherry bonsai font",
+     description:"A font for creating bonsai cherries. Ryan Holmberg wrote an article on the Typepicter a couple of days ago. A Japanese typewriter from the early twentieth century, specifically for making artistic images - ASCII art of the time. I've created a font that imitates a few units of the Typepicter.",
+     link:"typepicter"
+    },
+    {type:"Miscellaneous",
+     date:"February 20, 2014",
+     machinedate: "20140220",
+     title:"Kierkegaard",
+     description:"The Søren Kierkegaard Research Center has kindly published the collected works of Kierkegaard online. Unfortunately they are not made available as e-books, but as XML. The notation kn1 is thoroughly documented. I have made a parser that generates e-books based on the XML.",
+     link:"kierkegaard"
+    },
+    {type:"Miscellaneous",
+     date:"February 19, 2014",
+     machinedate: "20140219",
+     title:"Drawings",
+     description:"Not having brought a computer with me on vacation this fall, I did something else than programming for once - drawing. It turned out to be surprisingly easy and pleasant to draw on a phone. Some took hours, others minutes. They were drawn on the Faroe Islands between hikes - hence the concentration of fish birds and rain.",
+     link:"drawings"
+    },
+    {type:"Projects",
+     date:"Begruary 18, 2014",
+     machinedate: "20140218",
+     title:"Thesis",
+     description:"In 2012 I wrote my thesis. The subject was computer vision and recognition of objects in videos. The user points at the object in the first frame, and the algorithm will attempt, without prior training, to recognize in real time for the remainder of the video. Below are the — slightly more technical — abstract and a sample video.",
+     link:"thesis"
+    }
+];
